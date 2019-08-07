8891cddae916806a98fc5ba5692b4c5386275139
Guilherme Takashi Yamamoto <guilhermetyamamoto@gmail.com>
2019-08-07 14:56:43 -0300

    wip

--- src/app/htmlelements/actions/elements.actions.factory.ts
+++ src/app/htmlelements/actions/elements.actions.factory.ts
@@ -4,5 +4,5 @@ import { createAction, props } from '@ngrx/store';
 
 
 export const initialize = createAction('[HtmlElement Component] Initialize', props<{ elementRef: string }>());
-export const load = createAction('[HtmlElement Component] Load', props<{ elementRef: string }>());
-export const unload = createAction('[HtmlElement Component] Unload', props<{ elementRef: string }>());
+export const load = createAction('[HtmlElement Component] Load', props<{ elementRef: string, parent: string }>());
+export const unload = createAction('[HtmlElement Component] Unload', props<{ elementRef: string, parent: string }>());
--- src/app/htmlelements/reducers/elements.reducers.ts
+++ src/app/htmlelements/reducers/elements.reducers.ts
@@ -4,32 +4,34 @@ export const initialState =
 {
   'header': {
     loaded: false,
-    state:'unloaded',
+    // state: 'unloaded',
     path: 'header/main.js',
     element: 'app-header'
   },
   'app1': {
     loaded: false,
-    state:'unloaded',
+    // state: 'unloaded',
     path: 'app1/main.js',
     element: 'app-one'
   }
 };
 
 export const htmlElementsReducer = createReducer(initialState,
-  on(initialize, (state, { elementRef }) => {
-    state[elementRef].loaded = false;
-    state[elementRef].state='init';
-    return state;
-  }),
-  on(load, (state, { elementRef }) => {
+  // on(initialize, (state, { elementRef }) => {
+  //   state[elementRef].loaded = false;
+  //   state[elementRef].state = 'init';
+  //   state[elementRef]
+  //   return state;
+  // }),
+  on(load, (state, { elementRef,parent }) => {
     state[elementRef].loaded = true;
-    state[elementRef].state='loaded';
+   // state[elementRef].state = 'loaded';
+
     return state;
   }),
-  on(unload, (state, { elementRef }) => {
+  on(unload, (state, { elementRef,parent }) => {
     state[elementRef].loaded = false;
-    state[elementRef].state='unloaded';
+    //state[elementRef].state = 'unloaded';
     return state;
   }),
 );
